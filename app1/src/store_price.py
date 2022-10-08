28e87d6b54766ffcc9f82701a5d4429919886037
Marc Partensky <marc.partensky@gmail.com>
2022-10-08 14:48:23 +0200

    clean store price.py

--- app1/src/store_price.py
+++ app1/src/store_price.py
@@ -16,33 +16,40 @@ Communication with the database is processed via the ms1
 
 Return the number of clonned items
 """
+
+
 def clone_product(product_id, new_product_id, coef):
     LIST_PRODUCT_URL = "http://ms1:8000/product_items"
-    items = requests.get(url = f"{LIST_PRODUCT_URL}/{product_id}").json()
-    
+    items = requests.get(url=f"{LIST_PRODUCT_URL}/{product_id}").json()
+
     ADD_PRODUCT_URL = "http://ms1:8000/product_item"
     for item in items:
-        del(item['id'])
-        item['product_id'] = new_product_id
-        item['price'] *= coef
-        requests.put(url = ADD_PRODUCT_URL, json = item)
+        del item["id"]
+        item["product_id"] = new_product_id
+        item["price"] *= coef
+        requests.put(url=ADD_PRODUCT_URL, json=item)
 
     return len(items)
+
 
 """
 Find the sum of items prices of a product
 """
+
+
 def sum_of_prices(product_id):
     LIST_PRODUCT_URL = "http://ms1:8000/product_items"
-    items = requests.get(url = f"{LIST_PRODUCT_URL}/{product_id}").json()
-    
-    return round(sum([i['price'] for i in items]))
+    items = requests.get(url=f"{LIST_PRODUCT_URL}/{product_id}").json()
+
+    return round(sum([i["price"] for i in items]))
 
 
 """
 Delete all product's items
 """
+
+
 def delete_product(product_id):
     LIST_PRODUCT_URL = "http://ms1:8000/product"
-    
-    return requests.delete(url = f"{LIST_PRODUCT_URL}/{product_id}").text
+
+    return requests.delete(url=f"{LIST_PRODUCT_URL}/{product_id}").text
