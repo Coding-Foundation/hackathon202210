86591069a91646f3a126f555708a502255f84cf6
Marc Partensky <marc.partensky@gmail.com>
2022-10-08 10:11:51 +0200

    facotrial

--- app1/src/fact.py
+++ app1/src/fact.py
@@ -1,19 +1,7 @@
-import sys
-""" 
-#### factorial function example ### 
- fact n 
- n integer number
- commande_type fact
- args : n , positive integer 
- in  case of of negative return 0
-"""
+import math
 
-def factorielle(a):
-    if a < 0:
-        return 'undefined'
-    if a < 2:
-        return 1
-    return a*factorielle(a-1)
+factorielle = math.factorial
+
 
 def cmd_fact(n):
     return str(factorielle(n))
