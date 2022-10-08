80df7f637ac772137c77b0d5a0d0e19ad869ace6
Marc Partensky <marc.partensky@gmail.com>
2022-10-08 17:23:58 +0200

    float

--- app1/src/store_price.py
+++ app1/src/store_price.py
@@ -53,7 +53,7 @@ def sum_of_prices(product_id):
     print(str(products)[:500])
     product_id = str(product_id)
     items = products[product_id]
-    return round(sum(int(item[2]) for item in items))
+    return round(sum(float(item[2]) for item in items))
 
 
 """
