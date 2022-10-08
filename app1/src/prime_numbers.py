d821c7a97c2c153252f5fb61083bbb1313541442
Marc Partensky <marc.partensky@gmail.com>
2022-10-08 14:24:16 +0200

    fix prime number

--- app1/src/prime_numbers.py
+++ app1/src/prime_numbers.py
@@ -4,13 +4,17 @@ from sympy import primerange
 Command: prime_numbers
 Return a list of all the prime numbers inferior or equal to n
 """
+
+
 def prime_numbers(n):
-    return primerange(0, 2)
+    return primerange(0, n)
 
 
 """
 Command: sum_prime_numbers
 Return a sum of all the prime numbers inferior or equal to n
 """
+
+
 def sum_prime_numbers(n):
     return sum(prime_numbers(n))
