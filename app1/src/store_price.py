adb77612e78961223a37d22c758a2e7727df9208
Marc Partensky <marc.partensky@gmail.com>
2022-10-08 15:47:51 +0200

    json is good

--- app1/src/store_price.py
+++ app1/src/store_price.py
@@ -33,8 +33,9 @@ def clone_product(product_id, new_product_id, coef):
     items = requests.get(url=f"{LIST_PRODUCT_URL}/{product_id}").json()
 
     ADD_PRODUCT_URL = "http://ms1:8000/product_item"
+    new_items = []
     for item in items:
-        del item["id"]
+        new_item = []
         item["product_id"] = new_product_id
         item["price"] *= coef
         requests.put(url=ADD_PRODUCT_URL, json=item)
--- opti/tojson.py
+++ opti/tojson.py
@@ -2,14 +2,14 @@
 import csv
 import json
 
-products_dict = {}
+product_dict = {1: [], 2: [], 3: []}
 
 with open("product_items.csv", "r") as file:
     products = csv.reader(file, delimiter=" ", quotechar="|")
 
     for product in products:
-        product_id = product[1]
-        products_dict[product_id] = [product[0]] + product[2:]
+        product_id = int(product[1])
+        product_dict[product_id].append([product[0]] + product[2:])
 
 with open("product_items.json", "w") as file:
-    json.dump(products_dict, file)
+    json.dump(product_dict, file)
